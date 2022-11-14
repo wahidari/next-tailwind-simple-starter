01123aebb2eb8e555388594f865a94c739a1d760
wahidari <wahiid.ari@gmail.com>
2022-11-15 06:46:03 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import { GlobalProvider } from "@utils/GlobalContext";
 import "@styles/globals.css";
+import { Inter } from '@next/font/google';
+import { ThemeProvider } from 'next-themes'
+
+const inter = Inter({ subsets: ['latin'] })
 
 function MyApp({ Component, pageProps }) {
   return (
-    <GlobalProvider>
-      <Component {...pageProps} />
-    </GlobalProvider>
+    <ThemeProvider attribute="class" storageKey='theme'>
+      {/* <GlobalProvider> */}
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+      {/* </GlobalProvider> */}
+    </ThemeProvider>
   )
 }
 
