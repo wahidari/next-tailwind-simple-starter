3bb8a893c6fc4b8c63a7a4e9c95ccb89d0dfa48a
wahidari <wahiid.ari@gmail.com>
2023-01-16 11:17:00 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    REPO_URL: "https://github.com/wahid-ari/next-tailwind-simple-starter",
+    // API_URL: "http://localhost:3000",
+  },
   reactStrictMode: true,
   images: {
     domains: ["images.unsplash.com"],
